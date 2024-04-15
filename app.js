342b48749e9a655458e400306118f1f47906fbb0
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-04-15 11:54:54 +0530

    test

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-
+// 1
 // using React Element
 const reactElement=React.createElement('h1',{id:"heading"},"Hello From React");
 
+// 2
 // Same thing via JSX
 const jsx=<h1>Hello from JSX</h1>;
 /*
-    JSX is an transpiller React element
+    JSX is an transpiller to React element
     JSX used a library 'Babel' in order to use as same a like HTML
     Note: JSX is not HTML code but act as same alike
 
@@ -28,13 +29,13 @@ const JsxComponent=()=>{
     return <h1>Hello from inside JSX Functions</h1>;
 }
 
-// same thing in different way to declare
+// same thing but different way to declare
 // First way
 const JsxComponentOne=()=> <h1>Hello from JSX function from singe line</h1>;
 
 // Second Way
 const JsxComponentTwo=()=>(
-    <h1>Hello from JSX using round braces</h1>
+    <h1>Hello from JSX using round  braces</h1>
 )
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
